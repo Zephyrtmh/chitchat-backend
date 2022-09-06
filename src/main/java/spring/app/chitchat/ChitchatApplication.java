c3a499b22c52bd455e486259220e0e05df9893af
Zephyr <Zephyrtmh@gmail.com>
2022-09-06 13:45:36 +0800

    first commit

---
+++ src/main/java/spring/app/chitchat/ChitchatApplication.java
@@ -0,0 +1,13 @@
+package spring.app.chitchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChitchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChitchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/app/chitchat/ChitchatApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.app.chitchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChitchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
